008904bffde103ed2f7c3e789fa3ffc0a7bc1652
love thakur <113324617+Lovethakur1@users.noreply.github.com>
2024-06-15 13:20:15 +0530

    1st push

---
+++ 01/index.js
@@ -0,0 +1,31 @@
+
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.get('/r', (req, res) => {
+  res.send('love');
+});
+
+app.get('/login' , (req, res) => {
+  res.send('<h1>login page</h1>'); 
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
